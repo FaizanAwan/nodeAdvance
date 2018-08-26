eacb2f0ad2005a23f9509126c20f1126bcac6e11
unknown <faizanasif117@gmail.com>
2018-08-26 22:02:40 +0500

    upload 2nd

--- test/header.test.js
+++ test/header.test.js
@@ -1,59 +1,70 @@
 const puppeteer = require('puppeteer');
 
-let browser,page;
 
-beforeEach(async()=>{
+let browser, page;
 
-     browser = await puppeteer.launch({
-        headless:false
+beforeEach(async () => {
+
+    browser = await puppeteer.launch({
+        headless: false
     }
     );
 
-     page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto('localhost:3000');
 
-},20000);
-
-afterEach(async()=>{
-    await browser.close();
 });
 
-test("Open Browser",async () => {
-
-    
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
-
-    expect(text).toEqual('Blogster');
-
+afterEach(async () => {
+    // await browser.close();
 });
 
-test("Click and check url",async () => {
+// test("Open Browser", async () => {
 
-    
-    await page.click('.right a')
 
-    const url = page.url();
-    expect(url).toMatch(/accounts\.google\.com/);
-});
+//     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
-test('check session',async()=> {
+//     expect(text).toEqual('Blogster');
+
+// });
+
+// test.only("Click and check url", async () => {
+
+
+//     await page.click('.right a')
+
+//     const url = page.url();
+//     expect(url).toMatch(/accounts\.google\.com/);
+// });
+
+test.only('check session', async () => {
 
     const id = '5b49d37e33797d21dc486c12';
-
+    jest.setTimeout(10000)
     const Buffer = require('safe-buffer').Buffer;
     const sessionObject = {
-        passport:{
+        passport: {
             user: id
         }
     };
-
+    
     const sessionString = Buffer.from(
         JSON.stringify(sessionObject)).toString('base64');
 
     const Keygrip = require('keygrip');
     const keys = require('../config/keys');
     const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session='+sessionString);
-    console.log(sessionString,sig);
+    const sig = keygrip.sign('session=' + sessionString);
+    console.log(sessionString, sig);
+
+    
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout/"]');
+
+    const text = await page.$eval('a[href="/auth/logout/"]',el=>el.innerHTML)
+    expect(text).toEqual('Logout');
 
 });
